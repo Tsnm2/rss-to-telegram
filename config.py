6eebec4b17e4d18885375c30238e955e66aab5af
Tsnm2 <90688335+Tsnm2@users.noreply.github.com>
2021-09-14 23:15:55 +0530

    Update and rename config.example.py to config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+This is a sample config file
+Copy this to config.py and fill in your actual data before starting
+"""
+
+config = {
+    "telegram-token": "1990575948:AAGIUQDT-MIf-p8oSNffBWkBsC_ys4bFKeE",
+    "channel-id": "-1001531113599",
+    "feeds": ["https://yts.mx/rss/0/all/all/0/all", "https://torrentgalaxy.to/rss"]
+}
